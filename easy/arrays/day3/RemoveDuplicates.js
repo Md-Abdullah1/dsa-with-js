11c064347de35ab805fd696d594076ad91a5c295
Md-Abdullah1 <abdullahahil153@gmail.com>
2025-11-14 02:51:59 +0530

    completed removing of array duplication

--- easy/arrays/day3/RemoveDuplicates.js
+++ easy/arrays/day3/RemoveDuplicates.js
@@ -3,20 +3,14 @@
 // Consider the number of unique elements in nums to be k​​​​​​​​​​​​​​. After removing duplicates, return the number of unique elements k.
 // The first k elements of nums should contain the unique numbers in sorted order. The remaining elements beyond index k - 1 can be ignored.
 var removeDuplicates = function (nums) {
-  let newNums = Array.from(new Set(nums));
-  let k = 0;
-  if (newNums.length != nums.length) {
-    for (let i = 0; i < nums.length; i++) {
-      if (i > newNums.length - 1) {
-        k = i;
-        newNums.push("_");
-      }
+  let k = 1;
+  if (nums.length == 0) return 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] != nums[k - 1]) {
+      nums[k] = nums[i];
+      k++;
     }
-    console.log(k, "in if ", newNums, nums);
-    // return (newNums.length , newNums)
-    return k;
-  } else {
-    console.log("in else ", newNums, nums);
-    return nums.length, nums;
   }
+  return k;
 };
